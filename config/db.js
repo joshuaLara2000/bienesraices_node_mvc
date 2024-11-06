4ff5e794050d9cad0d9fac100d7f9c41f60d04bb
joshuaLara2000 <joshua.guerrero@bluepixel.mx>
2024-11-05 19:18:10 -0600

    fix

--- config/db.js
+++ config/db.js
@@ -1,22 +1,28 @@
 import Sequelize from "sequelize";
-import dotenv from 'dotenv';
-dotenv.config({path: '.env'});
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-                        // Base de datos         nombre de usuario     password               objeto de configuración
-const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASS ?? '', {
+// Base de datos         nombre de usuario     password               objeto de configuración
+const db = new Sequelize(
+  process.env.DB_NOMBRE,
+  process.env.DB_USER,
+  process.env.DB_PASS ?? "",
+  {
     host: process.env.DB_HOST,
-    port: 3306,
-    dialect: 'mysql',
+    port: 3307,
+    dialect: "mysql",
     define: {
-        timestamp: true // Cuando un usuario se registra agrega dos columnas extras a la tabla de usuario (cuando fue creado/actualizado)
+      timestamp: true, // Cuando un usuario se registra agrega dos columnas extras a la tabla de usuario (cuando fue creado/actualizado)
     },
-    pool: { // Configura como va a ser el comportamiento para conexiones nuevas o existentes
-        max: 5, // Máximo de conexiones de mantener
-        min: 0, // Minimo de concexiones a mantener
-        acquire: 30000, // Los segundos que va a pasar tratando de elaborar una conexión antes de marcar un error
-        idle: 10000 // Los segundos en lo que ve que no hay nada de movimiento, si no hay visitas da 10s para que la conexión finalice
+    pool: {
+      // Configura como va a ser el comportamiento para conexiones nuevas o existentes
+      max: 5, // Máximo de conexiones de mantener
+      min: 0, // Minimo de concexiones a mantener
+      acquire: 30000, // Los segundos que va a pasar tratando de elaborar una conexión antes de marcar un error
+      idle: 10000, // Los segundos en lo que ve que no hay nada de movimiento, si no hay visitas da 10s para que la conexión finalice
     },
-    operatorAliases: false // Era algo que se utlizaba hace tiempo, pero ya no se usan, pero para asegurar que no los use, lo marcamos como false
-});
+    operatorAliases: false, // Era algo que se utlizaba hace tiempo, pero ya no se usan, pero para asegurar que no los use, lo marcamos como false
+  }
+);
 
 export default db;
